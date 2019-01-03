4245f2aa33906605fd3e93be6ea38a77305cf93f
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:20:37 +0700

    Add files via upload

---
+++ Menghitung luas Lingkarang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int r;
+    float phi=3.14,luas;
+    cout<<"MENGHITUNG LUAS LINGKARAN"<<endl;
+    cout<<"Masukan jari-jari lingkaran : " ;
+    cin>>r;
+    luas=phi*r*r;
+    cout<<"Luas lingkaran adalah : "<<luas<<endl;
+    return 0;
+}
